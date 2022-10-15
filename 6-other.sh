697744ae2ebef3609daaf02ffc98eab1c5a67319
mk <mehamednews@gmail.com>
2022-10-15 14:09:37 +0100

    add postman-bin

--- 6-other.sh
+++ 6-other.sh
@@ -4,7 +4,7 @@ yay -S ffmpeg mkvtoolnix-cli --noconfirm
 # font for VsCode
 yay -S ttf-jetbrains-mono --noconfirm
 
-yay -S visual-studio-code-bin google-chrome obs-studio obsidian vlc nordvpn-bin audiorelay telegram-desktop signal-desktop figma-linux-bin --noconfirm
+yay -S visual-studio-code-bin google-chrome obs-studio obsidian vlc nordvpn-bin audiorelay telegram-desktop signal-desktop figma-linux-bin postman-bin --noconfirm
 sudo usermod -aG nordvpn $USER
 
 sudo pacman -S remmina discord flameshot --noconfirm
